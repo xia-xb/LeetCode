003d7d4c8c651676c2f3b6c53d4f05024bf2e3b6
xia-xb <2276905732@qq.com>
2021-06-14 23:34:45 +0800

    commit 33

--- 二分查找应用（中等）.cpp
+++ 二分查找应用（中等）.cpp
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2021-06-05 21:02:55
- * @LastEditTime: 2021-06-09 22:40:38
+ * @LastEditTime: 2021-06-14 23:33:56
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: \LeetCode\二分查找应用（中等）.cpp
@@ -223,5 +223,125 @@ public:
         }
         sort(length.begin(), length.end());
         return length.back();
+    }
+};
+/* 动态优化+二分查找 */
+/* 通过定义一个tail数组，记录相同长度子序列的最小尾元素 */
+class Solution {
+public:
+    int lengthOfLIS(vector<int>& nums) {
+        vector<int> tail;
+        for (int i = 0; i < nums.size(); i++) {
+            int left = 0, right = tail.size() - 1, target = nums[i];
+            if (tail.empty() || target > tail.back()) {
+                tail.push_back(target);
+                continue;
+            }
+            while (left <= right) {
+                int mid = left + (right - left) / 2;
+                if (tail[mid] == target) {
+                    left = mid;
+                    break;
+                } else if (tail[mid] < target) {
+                    left = mid + 1;
+                } else {
+                    right = mid - 1;
+                }
+            }
+            if (left >= tail.size() || tail[left] == target) {
+                continue;
+            } else {
+                tail[left] = target;
+            }
+        }
+        return tail.size();
+    }
+};
+
+/* 162题寻找峰值 */
+/* 二分查找 */
+/* 利用相邻两元素不相等 */
+/* 根据相邻两元素关系判断处于上升、下降坡度中 */
+/* 从而改变寻找范围 */
+/* 与一般二分查找不同的是，其循环条件是left<right */
+/* 当两个相等时，则找到峰值 */
+class Solution {
+public:
+    int findPeakElement(vector<int>& nums) {
+        int left = 0, right = nums.size() - 1;
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] < nums[mid + 1]) {
+                left = mid + 1;
+            } else {
+                right = mid;
+            }
+        }
+        return left;
+    }
+};
+
+/* 4题寻找两个正序数组的中位数 */
+/* 主要问题是如何找到两个正序数组的第k个数 */
+/* 设置start1,start2为两个数组的指针 */
+/* 同时设置left,right分别记录当前值，和前一个值 */
+/* 若是找第k个数则只需要设置left即可 */
+class Solution {
+public:
+    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
+        int sum = nums1.size() + nums2.size();
+        int position = sum / 2 + 1;
+        int left = 0, right = 0, start1 = 0, start2 = 0;
+        while (position--) {
+            left = right;
+            if (start1 >= nums1.size()) {
+                right = nums2[start2];
+                start2++;
+            } else if (start2 >= nums2.size()) {
+                right = nums1[start1];
+                start1++;
+            } else if (nums1[start1] >= nums2[start2]) {
+                right = nums2[start2];
+                start2++;
+            } else {
+                right = nums1[start1];
+                start1++;
+            }
+        }
+        if (sum % 2) {
+            return right;
+        } else {
+            return (left + right) / 2.0;
+        }
+    }
+};
+/* 两个正序数组中寻找第k个元素算法改进 */
+/* 使用二分查找 */
+class Solution {
+public:
+    int getKth(vector<int>& nums1, int start1, vector<int>& nums2, int start2,
+               int k) {
+        if (start1 >= nums1.size()) {
+            return nums2[start2 + k - 1];
+        }
+        if (start2 >= nums2.size()) {
+            return nums1[start1 + k - 1];
+        }
+        if (k == 1) {
+            return min(nums1[start1], nums2[start2]);
+        }
+        int step = min(nums1.size() - start1, nums2.size() - start2);
+        step = min(step, k / 2);
+        if (nums1[start1 + step - 1] <= nums2[start2 + step - 1]) {
+            return getKth(nums1, start1 + step, nums2, start2, k - step);
+        }
+        return getKth(nums1, start1, nums2, start2 + step, k - step);
+    }
+    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
+        int sum = nums1.size() + nums2.size();
+        int position1 = (sum + 1) / 2, position2 = (sum + 2) / 2;
+        return (getKth(nums1, 0, nums2, 0, position1) +
+                getKth(nums1, 0, nums2, 0, position2)) /
+               2.0;
     }
 };
