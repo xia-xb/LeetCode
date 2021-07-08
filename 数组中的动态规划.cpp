926e8339438ed253194bae78e99a0cfff3f0f5db
xia-xb <2276905732@qq.com>
2021-07-08 22:46:26 +0800

    commit 55

--- 数组中的动态规划.cpp
+++ 数组中的动态规划.cpp
@@ -2,7 +2,7 @@
  * @Author: 夏玄兵
  * @Date: 2021-07-07 22:17:09
  * @LastEditors: 夏玄兵
- * @LastEditTime: 2021-07-07 23:01:41
+ * @LastEditTime: 2021-07-08 22:45:42
  * @Description: file content
  * @FilePath: \LeetCode\数组中的动态规划.cpp
  */
@@ -89,5 +89,45 @@ public:
             curr = sum;
         }
         return curr;
+    }
+};
+
+/* 338题比特位计数 */
+/* 动态规划 */
+/* dp0=0 */
+/* dp(i)=dp(i/2)    i为偶 */
+/* dp(i)=dp(i-1)+1  i为奇 */
+class Solution {
+public:
+    vector<int> countBits(int n) {
+        vector<int> dp(n + 1, 0);
+        for (int i = 1; i <= n; i++) {
+            if (i & 1) {
+                dp[i] = dp[i - 1] + 1;
+            } else {
+                dp[i] = dp[i / 2];
+            }
+        }
+        return dp;
+    }
+};
+
+/* 45题跳跃游戏II */
+/* 暴力搜索 */
+class Solution {
+public:
+    int jump(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> dp(n, INT_MAX);
+        dp[0] = 0;
+        for (int i = 0; i < n; i++) {
+            for (int k = 1; k <= nums[i] && k + i < n; k++) {
+                if (dp[k + i] != INT_MAX) {
+                    continue;
+                }
+                dp[k + i] = min(dp[k + i], 1 + dp[i]);
+            }
+        }
+        return dp.back();
     }
 };
