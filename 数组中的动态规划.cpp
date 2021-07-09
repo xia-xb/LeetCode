24c6ca85bdbfdd56caaebbf5d2472de6c6a371e2
xia-xb <2276905732@qq.com>
2021-07-09 23:45:04 +0800

    commit 56

--- 数组中的动态规划.cpp
+++ 数组中的动态规划.cpp
@@ -2,7 +2,7 @@
  * @Author: 夏玄兵
  * @Date: 2021-07-07 22:17:09
  * @LastEditors: 夏玄兵
- * @LastEditTime: 2021-07-08 22:45:42
+ * @LastEditTime: 2021-07-09 23:44:27
  * @Description: file content
  * @FilePath: \LeetCode\数组中的动态规划.cpp
  */
@@ -129,5 +129,179 @@ public:
             }
         }
         return dp.back();
+    }
+};
+
+/* 198题打家劫舍 */
+/* 动态规划 */
+/* dp[i]打劫第i家的最大金额 */
+/* dp[0]=nums[0],dp[i]=nums[1] */
+/* dp[i]=nums[i]+max(dp[j])  (0<=j<i-1) */
+
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        if (nums.size() == 1) {
+            return nums[0];
+        }
+        if (nums.size() == 2) {
+            return max(nums[0], nums[1]);
+        }
+        vector<int> dp(nums.size(), 0);
+        dp[0] = nums[0];
+        dp[1] = nums[1];
+        int res = -1;
+        int maxPre = dp[0];
+        for (int i = 2; i < nums.size(); i++) {
+            dp[i] = nums[i] + maxPre;
+            res = max(dp[i], dp[i - 1]);
+            maxPre = max(dp[i - 1], maxPre);
+        }
+        return res;
+    }
+};
+/* 优化内存空间 */
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        if (nums.size() == 1) {
+            return nums[0];
+        }
+        if (nums.size() == 2) {
+            return max(nums[0], nums[1]);
+        }
+        int res = -1;
+        int maxPre = nums[0], pre = nums[1];
+        for (int i = 2; i < nums.size(); i++) {
+            int curr = nums[i] + maxPre;
+            res = max(curr, pre);
+            maxPre = max(pre, maxPre);
+            pre = curr;
+        }
+        return res;
+    }
+};
+/* dp[i]表示前i+1间房能打劫的最大金额 */
+/* 对于dp[i]打劫则为nums[i]+dp[i-2] */
+/* 不打劫为dp[i-1] */
+/* 所以为两个的最大值 */
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        if (nums.empty()) {
+            return 0;
+        }
+        if (nums.size() == 1) {
+            return nums[0];
+        }
+        if (nums.size() == 2) {
+            return max(nums[0], nums[1]);
+        }
+        vector<int> dp(nums.size(), 0);
+        dp[0] = nums[0];
+        dp[1] = max(nums[0], nums[1]);
+        for (int i = 2; i < nums.size(); i++) {
+            dp[i] = max(dp[i - 2] + nums[i], dp[i - 1]);
+        }
+        return max(dp.back(), dp[dp.size() - 2]);
+    }
+};
+/* 内存优化 */
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        if (nums.empty()) {
+            return 0;
+        }
+        if (nums.size() == 1) {
+            return nums[0];
+        }
+        if (nums.size() == 2) {
+            return max(nums[0], nums[1]);
+        }
+        int first = nums[0], second = max(nums[0], nums[1]);
+        for (int i = 2; i < nums.size(); i++) {
+            int cur = max(first + nums[i], second);
+            first = second;
+            second = cur;
+        }
+        return max(first, second);
+    }
+};
+
+/* 213题打家劫舍II */
+/* 两次动态规划，分为0打劫或不打劫 */
+class Solution {
+public:
+    int maxAmount(vector<int>& nums, vector<int>& dp) {
+        int n = nums.size();
+        if (dp[0] >= 0) {
+            dp[1] = max(nums[0], nums[1]);
+            for (int i = 2; i < n - 1; i++) {
+                dp[i] = max(nums[i] + dp[i - 2], dp[i - 1]);
+            }
+            return max(dp[n - 2], dp[n - 3]);
+        } else {
+            dp[1] = nums[1];
+            dp[2] = max(nums[1], nums[2]);
+            for (int i = 3; i < n; i++) {
+                dp[i] = max(nums[i] + dp[i - 2], dp[i - 1]);
+            }
+            return max(dp[n - 2], dp[n - 1]);
+        }
+    }
+    int rob(vector<int>& nums) {
+        if (nums.empty()) {
+            return 0;
+        }
+        if (nums.size() == 1) {
+            return nums[0];
+        }
+        if (nums.size() == 2) {
+            return max(nums[0], nums[1]);
+        }
+        vector<int> dp(nums.size(), -1);
+        int res1 = maxAmount(nums, dp);
+        vector<int> dp1(nums.size(), -1);
+        dp1[0] = nums[0];
+        int res2 = maxAmount(nums, dp1);
+        return max(res1, res2);
+    }
+};
+/* 内存优化 */
+class Solution {
+public:
+    int maxAmount(vector<int>& nums, int first) {
+        int n = nums.size();
+        if (first >= 0) {
+            int second = max(nums[0], nums[1]);
+            for (int i = 2; i < n - 1; i++) {
+                int cur = max(nums[i] + first, second);
+                first = second;
+                second = cur;
+            }
+            return max(first, second);
+        } else {
+            first = nums[1];
+            int second = max(nums[1], nums[2]);
+            for (int i = 3; i < n; i++) {
+                int cur = max(nums[i] + first, second);
+                first = second;
+                second = cur;
+            }
+            return max(first, second);
+        }
+    }
+    int rob(vector<int>& nums) {
+        if (nums.empty()) {
+            return 0;
+        }
+        if (nums.size() == 1) {
+            return nums[0];
+        }
+        if (nums.size() == 2) {
+            return max(nums[0], nums[1]);
+        }
+        return max(maxAmount(nums, nums[0]), maxAmount(nums, -1));
     }
 };
